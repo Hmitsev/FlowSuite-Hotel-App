42b6b1aa4d0f64f3f177bdb9037932da749d45d1
Hmitsev <hmitsev@intercars.eu>
2026-09-21 18:47:31 +0300

    Update translations.py

--- translations.py
+++ translations.py
@@ -108,9 +108,9 @@ TRANSLATIONS = {
         # Home page
         "welcome": "Welcome",
         "hotel_services": "Hotel Services",
-        "room_service": "Рум Сървиз",
+        "room_service": "Room_Service",
         "room_service_description": "Food and drinks",
-        "spa": "СПА",
+        "spa": "SPA",
         "spa_description": "Massages and treatments",
         "activities": "Activities",
         "activities_description": "Sports and activities",
